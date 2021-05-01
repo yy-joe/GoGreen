2ed72cd9e1009d11329a5714c93116d255beaf74
yy-joe <82998432+yy-joe@users.noreply.github.com>
2021-05-01 15:05:28 +0800

    Updated the SQL Script
    Added/modified:
    Product image: VARCHAR for the image location path
    Product status: live/ sold out/ discontinued/ coming soon/ awaiting stock/ backorder, etc
    Product short description
    Product long description
    Product date type
    Product condition: used/new
    Foreign key auto delete: cascade on update/delete
    User table: additional field:
    -Date joined

---
+++ GoGreenDB.sql
@@ -0,0 +1,72 @@
+/*create database GoGreen;*/
+create database GoGreen;
+use GoGreen;
+create table `Users` (
+	`ID` int not null auto_increment, 
+    `Username` varchar(30) not null, 
+    `Password` varchar(20) not null, 
+    `Name` varchar(50) not null, 
+    `Role` varchar(30) not null, 
+    `Email` varchar(100) not null, 
+    `Address` varchar(100) not null, 
+    `Contact` varchar(8) not null,
+    `Date_Joined` date not null,
+    Primary Key (`ID`)
+);
+
+CREATE TABLE `Categories` (
+  `ID` int NOT NULL auto_increment,
+  `Name` varchar(200) NOT NULL,
+  `Description` varchar(500),
+  `Number_Of_Products` int not null,
+  Primary Key (`ID`)
+);
+
+CREATE TABLE `Brands` (
+  `ID` int NOT NULL auto_increment,
+  `Name` varchar(200) NOT NULL,
+  `Description` varchar(500),
+  `Number_Of_Products` int not null,
+  Primary Key (`ID`)
+);
+
+CREATE TABLE `Products` (
+  `ID` int NOT NULL auto_increment,
+  `Name` varchar(200) NOT NULL,
+  `Image` varchar(100), /*just the path of the image location on the server*/
+  `Desc_Short` varchar(500) NOT NULL,
+  `Desc_Long` varchar(1000),
+  `Date_Created` date not null,
+  `Date_Modified` date not null,
+  `Price` decimal(10,2) NOT NULL,
+  `Quantity` int NOT NULL,
+  `Condition` varchar(10) NOT NULL,
+  `Category_ID` int NOT NULL,
+  `Brand_ID` int NOT NULL,
+  `Status` varchar(100) NOT NULL, /*Status can be: live, sold out, or discontinued*/
+  Primary Key (`ID`),
+  Foreign Key(`Category_ID`) references Categories(`ID`) on update cascade on delete cascade,
+  Foreign Key(`Brand_ID`) references Brands(`ID`) on update cascade on delete cascade
+);
+
+CREATE TABLE `Customer_Orders` (
+  `ID` int NOT NULL auto_increment,
+  `Status` varchar(200) NOT NULL,
+  `User_ID` int NOT NULL,
+  `Amount` decimal(10,2) NOT NULL,
+  `Order_Date` date not null,
+  `Delivery_Date` date not null,
+  Primary Key (`ID`),
+  Foreign Key(`User_ID`) references Users(`ID`) on update cascade on delete cascade
+);
+
+CREATE TABLE `Product_Orders` (
+  `ID` int NOT NULL auto_increment,
+  `Status` varchar(200) NOT NULL,
+  `Quantity` int not null,
+  `Product_ID` int NOT NULL,
+  `Customer_Order_ID` int NOT NULL,
+  Primary Key (`ID`),
+  Foreign Key(`Product_ID`) references Products(`ID`) on update cascade on delete cascade,
+  Foreign Key(`Customer_Order_ID`) references Customer_Orders(`ID`) on update cascade on delete cascade
+);
