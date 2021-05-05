69cc0a3c30a0e096f5f39a3b038f008ca3aa2c72
plsbcool <plsbcool@gmail.com>
2021-05-05 09:26:05 +0800

    Add files via upload
    Still working on redis

---
+++ go.mod
@@ -0,0 +1,14 @@
+module nigga.com/testing
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/go-redis/redis/v7 v7.4.0 // indirect
+	github.com/go-redis/redis/v8 v8.8.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/twinj/uuid v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,54 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/go-redis/redis/v8"
 )
 
-type User struct {
+// uuid "github.com/satori/go.uuid"
+// 	"golang.org/x/crypto/bcrypt"
+
+type loginDetails struct {
 	ID       int64  `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
+var bPassword, _ = bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+
 // default
-var user = User{
+var user = loginDetails{
 	ID:       1,
 	Username: "admin",
 	Password: "password",
 }
 
+type TokenDetails struct {
+	AccessToken string
+	AccessUuid  string
+	AtExpires   int64
+}
+
 var key = ""
+var redisdb *redis.Client
+var ctx = context.Background()
 
 func init() {
 	err := godotenv.Load()
@@ -35,10 +56,40 @@ func init() {
 		log.Fatal("Error loading .env file")
 	}
 	key = os.Getenv("SECRET_KEY")
+
+	//Initializing redis
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	redisdb = redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+	// _, err = client.Ping().Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
 
-// var mySigningKey = []byte("c78afaf-97da-4816-bbee-9ad239abb296")
+// save meta
+func CreateAuth(userid int64, td *TokenDetails) error {
+	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
+	// rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
 
+	errAccess := redisdb.Set(ctx, td.AccessUuid, strconv.Itoa(int(userid)), at.Sub(now)).Err()
+	// errAccess := client.Set(td.AccessUuid, at.Sub(now)).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+	// errRefresh := client.Set(td.RefreshUuid, strconv.Itoa(int(userid)), rt.Sub(now)).Err()
+	// if errRefresh != nil {
+	// 	return errRefresh
+	// }
+	return nil
+}
+
+// Parse, verify and return
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
@@ -65,15 +116,6 @@ func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handle
 	})
 }
 
-// func loginFunc(x string, y string) map[string]interface {
-
-// }
-
-func testtest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
 func home(w http.ResponseWriter, r *http.Request) {
 	// validToken, err := CreateToken(1)
 	// if err != nil {
@@ -84,15 +126,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Failed to generate token")
 	}
-	r.Header.Set("Token", validToken)
-	body, err := ioutil.ReadAll(r.Body)
+	r.Header.Set("Token", validToken.AccessToken)
+
+	_, err = ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Fprintf(w, string(body))
-	fmt.Fprintf(w, validToken)
+	// fmt.Fprintf(w, string(body))
 	fmt.Fprintf(w, "Hello World")
 	fmt.Println("Endpoint Hit: homePage")
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -100,72 +151,69 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 
 	}
-	var thisUser User
+	var thisUser loginDetails
 	err1 := json.Unmarshal(body, &thisUser)
 	if err1 != nil {
 
 	}
-
 	// debugging
 	fmt.Println(thisUser.Username, thisUser.Password)
-	// login := loginFunc(user.Username, user.Password)
+
 	if user.Username != thisUser.Username || user.Password != thisUser.Password {
 		// err
 		w.Write([]byte("Unauthorised.\n"))
 	}
 
 	token, err := GenerateJWT()
+
 	if err != nil {
 		w.Write([]byte("Wrong token.\n"))
 	}
 	fmt.Println("access granted")
-	json.NewEncoder(w).Encode(token)
+
+	tokens := map[string]string{
+		"access_token": token.AccessToken,
+	}
+
+	json.NewEncoder(w).Encode(tokens)
 }
 
-func GenerateJWT() (string, error) {
-	// os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
-	// atClaims := jwt.MapClaims{}
-	// atClaims["authorized"] = true
-	// atClaims["user_id"] = userid
-	// atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-	// at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	// token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+func GenerateJWT() (*TokenDetails, error) {
+	// token details
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 30).Unix()
+	u := uuid.NewV4()
+	td.AccessUuid = u.String()
+	// td.AccessUuid = uuid.NewV4().String()
 
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["authorized"] = true
-	claims["client"] = "Elliot Forbes"
-	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	claims["access_uuid"] = td.AccessUuid
+	claims["exp"] = td.AtExpires
 
-	tokenString, err := token.SignedString([]byte(key))
+	var err error
+
+	td.AccessToken, err = token.SignedString([]byte(key))
 
 	if err != nil {
 		fmt.Errorf("Something Went Wrong: %s", err.Error())
-		return "", err
+		return nil, err
 	}
 
-	return tokenString, nil
+	return td, nil
 }
 
 func main() {
-	fmt.Println(key)
-	fmt.Println(mySigningKey)
-	// test, err := CreateToken()
+	// token2, err := GenerateJWT()
 	// if err != nil {
 
 	// }
-	// fmt.Println(test)
-
-	token2, err := GenerateJWT()
-	if err != nil {
-
-	}
-	fmt.Println(token2)
+	// fmt.Println(token2)
 	router := mux.NewRouter()
 	router.Handle("/home", isAuthorized(home)).Methods("GET")
-	router.HandleFunc("/test", testtest).Methods("GET")
 	// router.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) { isAuthorized(test, home) }).Methods("GET")
 	// getR := router.Methods(http.MethodGet).Subrouter()
 	// getR.HandleFunc("/home", home).Methods("GET")
